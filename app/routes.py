9cf7ea6351fe9a333f706ffdc1ab6b12dcb771a8
WiktorKarnia <wiktor1341@o2.pl>
2020-04-29 14:59:18 +0200

    WK 2020.04.29

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,28 @@
 from app import app
 from flask import render_template
+from flaskext.markdown import Markdown
+from app.forms import ProductForm
+app.config['SECRET_KEY'] = "Tajemniczy_mysi_sprzęt"
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("layout.htm")
+    return render_template("layout.html")
 
-@app.route('/scraper')
-def scraper():
+@app.route("/about")
+def about():
+    content = ""
+    with open("README.md", "r" ,encoding="utf-8") as f:
+        content = f.read()
+    return render_template("about.html", text = content)
+
+@app.route("/extract")
+def extract():
+    form = ProductForm()
+    return render_template("extract.html", form = form)
+
+@app.route('/products')
+def products():
     return "Podaj kod produktu do pobrania opinii"
 
 @app.route('/analyzer/<product_id>')
